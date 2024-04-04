54f9ae7676764e0f18590717a8f2eb4486f53926
Jan Golder <kontakt.jangolder@gmail.com>
2024-04-04 20:16:13 +0200

    add universal validation

---
+++ src/util/validation.js
@@ -0,0 +1,17 @@
+// validation functions which always return boolean value
+
+export function isEmail(value) {
+  return value.includes('@');
+}
+
+export function isNotEmpty(value) {
+  return value.trim() !== '';
+}
+
+export function hasMinLength(value, minLength) {
+  return value.length >= minLength;
+}
+
+export function isEqualsToOtherValue(value, otherValue) {
+  return value === otherValue;
+}
